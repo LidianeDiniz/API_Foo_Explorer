d0160e2075528b5afbc4fabd56f9b77c3204e9db
lidiane <lidianefernandesdiniz.lfd@email.com>
2022-09-02 21:37:39 -0300

    modified

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 const { Router } = require ("express");
 
 const usersRouter = require("./users.routes")
-const notesRouter = require("./notes.routes")
-const tagsRouter = require("./tags.routes");
+const cartsRouter = require("./carts.routes")
+const platesRouter = require("./plates.routes");
 const sessionsRouter = require("./Sessions.routes");
 
 
@@ -10,8 +10,8 @@ const sessionsRouter = require("./Sessions.routes");
 const routes = Router();
 
 routes.use("/users", usersRouter);
-routes.use("/notes", notesRouter);
-routes.use("/tags", tagsRouter);
+routes.use("/carts", cartsRouter);
+routes.use("/plates", platesRouter);
 routes.use("/sessions", sessionsRouter);
 
 
--- src/server.js
+++ src/server.js
@@ -1,24 +1,24 @@
 require("express-async-errors");
 require('dotenv/config');
-const migrationsRun = require("./database/sqlite/migrations");
-const AppError = require("./utils/AppError");
+
+
+const sqliteConnection = require('./database/sqlite');
 const uploadConfig = require ("./configs/upload");
+const AppError = require("./utils/AppError");
+const express = require ("express");
+const routes = require("./routes");
 const cors = require("cors");
 
-const { request, response } = require("express");
-const express = require ("express");
-
-const routes = require("./routes");
-
-migrationsRun();
+sqliteConnection();
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-app.use(routes);
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 
+app.use(routes);
 
 app.use((error, request, response, next)=> {
 if(error instanceof AppError){
